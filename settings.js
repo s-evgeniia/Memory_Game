f59fbd1e3b8d7c0de003234dbf88b782e7adb96e
s-evgeniia <ev.sokolova@icloud.com>
2023-11-13 11:46:04 +0100

    add Initial Page

--- settings.js
+++ settings.js
@@ -3,4 +3,13 @@ const TIMEOUT = 800;
 const AppRoute = {
     Game: 'game',
     Results: 'results',
+    Initial: 'initial'
 };
+
+const GAME_TYPES = [
+    { type: 'cats', icon: 'cat', label: 'Котики' },
+    { type: 'flowers', icon: 'flow', label: 'Цветочки' },
+    { type: 'cars', icon: 'car', label: 'Машины' },
+];
+
+RANDOMIZED = true
